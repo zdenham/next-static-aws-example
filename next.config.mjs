128e886d0cc6e2df1a65949478c816794a9fe642
zdenham <zdenham1@gmail.com>
2024-06-03 21:29:12 -0700

    pages router

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  rewrites: async () => {
-    return [
-      {
-        source: '/id/:id',
-        destination: '/id/id',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
