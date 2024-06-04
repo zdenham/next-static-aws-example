a6232dac2fa751ccbdeb42e8d6de4aa38de8ae37
zdenham <zdenham1@gmail.com>
2024-06-04 16:37:28 -0700

    with dynamic

--- src/lib/withDynamicParams.ts
+++ src/lib/withDynamicParams.ts
@@ -1,7 +1,20 @@
+const FALLBACK_STRING = 'next-static';
+
 const cannotDeduceErr =
   'Cannot deduce path, please provide a path to withDynamicParams';
 
 const patternToMatch = /app\/(.*?)\.tsx/;
+
+export const withDynamicParams = (
+  staticParamsFunc?: ParamsFunc,
+  path: string = getPathFromErrorStack()
+): ParamsFunc => {
+  return async () => {
+    const staticParamsArr = staticParamsFunc ? await staticParamsFunc() : [];
+    const fallbackParams = getFallbackParamsFromPath(path);
+    return [...staticParamsArr, fallbackParams];
+  };
+};
 
 const parseStackLine = (stackLine: string): string => {
   const match = stackLine.match(patternToMatch);
@@ -28,12 +41,22 @@ const getPathFromErrorStack = (): string => {
   throw new Error(cannotDeduceErr);
 };
 
-export const withDynamicParams = (path: string = getPathFromErrorStack()) => {
-  return async () => {
-    return [
-      {
-        id: '1',
-      },
-    ];
-  };
+const getFallbackParamsFromPath = (
+  path: string
+): Record<string, typeof FALLBACK_STRING> => {
+  const pathParts = path.split('/');
+
+  const params = pathParts.map((part) => {
+    if (part.startsWith('[') && part.endsWith(']')) {
+      return FALLBACK_STRING;
+    }
+    return part.replace('[', '').replace(']', '');
+  });
+
+  return params.reduce((acc, param) => {
+    acc[param] = FALLBACK_STRING;
+    return acc;
+  }, {} as Record<string, typeof FALLBACK_STRING>);
 };
+
+type ParamsFunc = () => Promise<Record<string, string>[]>;
