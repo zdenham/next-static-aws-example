30bb83fa91059efe3b144db859109fd413af805a
zdenham <zdenham1@gmail.com>
2024-06-05 15:05:59 -0700

    fix error codes

--- src/components/ID.tsx
+++ src/components/ID.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useDynamicParams } from 'next-static-utils';
+import { useDynamicParams, FALLBACK_STRING } from 'next-static-utils';
 
 export function ID() {
   const { id } = useDynamicParams();
+  if (id === FALLBACK_STRING) return null;
   return <div>{id}</div>;
 }
