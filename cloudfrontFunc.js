3a275218bb5e099a7324aa888b5d121cda714fcd
zdenham <zdenham1@gmail.com>
2024-06-05 17:28:50 -0700

    cloudfront func

---
+++ cloudfrontFunc.js
@@ -0,0 +1,40 @@
+'use strict';
+const serveConfig = {
+  rewrites: [
+    {
+      source: '/test/:id',
+      destination: '/test/next-static-fallback.html',
+    },
+  ],
+};
+function handler(event) {
+  var request = event.request;
+  var uri = request.uri;
+  function isMatch(pattern, path) {
+    // Escape any special regex characters in the pattern
+    const escapedPattern = pattern.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    // Replace variable segments with a regex pattern that matches any non-slash characters
+    const regexPattern = new RegExp(
+      '^' + escapedPattern.replace(/\\:([^/]+)/g, '([^/]+)') + '$'
+    );
+    return regexPattern.test(path);
+  }
+  // handle rewrites
+  for (var i = 0; i < serveConfig.rewrites.length; i++) {
+    var rewrite = serveConfig.rewrites[i];
+    if (isMatch(rewrite.source, uri)) {
+      uri = rewrite.destination;
+      request.uri = uri;
+      break;
+    }
+  }
+  // handle trailing slash, and redirect to html if no extension
+  if (!uri.includes('.') && uri.length > 1) {
+    if (uri.endsWith('/')) {
+      request.uri = uri.replace(/\/?$/, '.html');
+    } else {
+      request.uri = uri + '.html';
+    }
+  }
+  return request;
+}
