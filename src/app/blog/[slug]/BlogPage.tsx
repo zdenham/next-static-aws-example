53345a52bd42ffe9393d0844808a1bd607de28b6
zdenham <zdenham1@gmail.com>
2024-06-06 20:38:01 -0700

    silly example content

--- src/app/blog/[slug]/BlogPage.tsx
+++ src/app/blog/[slug]/BlogPage.tsx
@@ -5,5 +5,19 @@ export default function BlogPage() {
 
   if (slug === FALLBACK_STRING) return null;
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is a dynamic page with slug: {slug}</h1>
+      <p>
+        Dynamic content can be fetched from a server client side to hydrate the
+        page. You can refresh the page, and it will not 404, even though no page
+        at /{slug}.html exists. This works due to cloudfront function routing
+        logic and a fallback route generated via the{' '}
+        <a href="https://github.com/zdenham/next-static-utils" target="_blank">
+          next-static-utils
+        </a>{' '}
+        cli .
+      </p>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
+'use client';
+
+import { useState } from 'react';
+
 import Link from 'next/link';
 
 export default function Home() {
+  const [dynamicSlug, setDynamicSlug] = useState('my-dynamic-slug');
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <Link href="test/banana">Go to /test/banana</Link>
+      <h1>Welcome!</h1>
+      <p>
+        This is a static next.js website hosted on s3 + cloudfront built using{' '}
+        <a href="https://github.com/zdenham/next-static-utils" target="_blank">
+          next-static-utils
+        </a>{' '}
+        . This site demonstrates dynamic url parameters using the app router.
+        Type in any slug below to visit a dynamic page.
+      </p>
+      <div>
+        <p>/blog/</p>
+        <input
+          onChange={(e) => setDynamicSlug(e.target.value)}
+          value={dynamicSlug}
+        ></input>
+        <Link href={`/blog/${dynamicSlug}`}>Go to Dynamic Page</Link>
+      </div>
     </div>
   );
 }
